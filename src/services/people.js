e489abd4040dcaa6fe3f2417a5dd40b38da7ba40
Zuoping Li <studytime@163.com>
2018-08-28 20:01:16 +0800

    search people

--- src/services/people.js
+++ src/services/people.js
@@ -7,12 +7,39 @@ export function getSamples() {
   return request.post(opts);
 }
 
-export function searchByNames(names) {
-  const opts = {
-    query: 'MATCH (son:Person)-[:RELATION*0..{role:"son"}]->(parent:Person)<-[:RELATION {role: "wife"}]-(wife:Person) WHERE son.名 In {names} RETURN distinct son, parent, wife',
-    params: {
-      names
+export function searchByNames(originNames) {
+  if (originNames.length > 2) {
+    originNames = originNames.slice(0, 2);
+  }
+
+  let queries = [];
+  originNames.forEach(name => {
+    let newName = name.replace(' ', '');
+    if (newName.indexOf('>') > -1) {
+      let newNames = newName.split('>');
+      if (newNames.length >= 2) {
+        let parentName = newNames[newNames.length - 2];
+        newName = newNames[newNames.length - 1];
+        queries.push(
+          `((son.名='${newName}' OR son.字='${newName}' OR son.号='${newName}' OR son.又='${newName}' )
+          AND
+          (parent.名='${parentName}' OR parent.字='${parentName}' OR parent.号='${parentName}' OR parent.又='${parentName}'))
+          `
+        );
+      }
+    } else {
+      queries.push(
+        `(son.名='${newName}' OR son.字='${newName}' OR son.号='${newName}' OR son.又='${newName}' )`
+      );
     }
+    return newName;
+  });
+  const opts = {
+    query: `MATCH (son:Person)-[:RELATION*0..{role:"son"}]->(parent:Person)<-[:RELATION {role: "wife"}]-(wife:Person) WHERE ${queries.join(
+      ' OR '
+    )} RETURN distinct son, parent, wife ORDER BY id(parent) DESC`
   };
+  // console.log(opts);
+
   return request.post(opts);
 }
--- src/store/reducers/people.js
+++ src/store/reducers/people.js
@@ -1,8 +1,19 @@
 import { handleActions } from 'redux-actions';
-import { ACTIVE_SEARCH_PEOPLE, SEARCH_PEOPLE } from '../types/people';
+import {
+  ACTIVE_SEARCH_PEOPLE,
+  SEARCH_PEOPLE,
+  SET_SEARCH_KEYWORDS
+} from '../types/people';
 
 export default handleActions(
   {
+    [SET_SEARCH_KEYWORDS](state, action) {
+      // console.log('searching...', action.payload.keywords);
+      return {
+        ...state,
+        keywords: action.payload.keywords
+      };
+    },
     [ACTIVE_SEARCH_PEOPLE](state, action) {
       return {
         ...state,
@@ -10,10 +21,52 @@ export default handleActions(
       };
     },
     [SEARCH_PEOPLE](state, action) {
-      console.log(state, action);
+      // console.log(state, action);
+      // construct data like
+      /*
+      {
+        '作平': [
+          {名：'作平'},
+          {名：'良系', 妻: [
+            {名: '郭氏'}
+          ]},
+          {{名：'会金', 妻: [
+            {名: '吴氏'}
+          ]}
+        ],
+        '作苏': [
+         ...
+        ]
+      }
+      */
+      let array = {};
+      action.payload.data.data.forEach((v, i) => {
+        const son = v[0].data,
+          parent = v[1].data,
+          wife = v[2].data;
+        let name = son.名;
+        if (!array[name]) {
+          array[name] = [son];
+        }
+        const itemInArray = array[name].find(s => {
+          return s.名 === parent.名;
+        });
+        let newItem = {};
+        if (!itemInArray) {
+          newItem = parent;
+          newItem.妻 = [wife];
+          array[name].push(newItem);
+        } else {
+          newItem = itemInArray;
+          if (!newItem.妻) {
+            newItem['妻'] = [];
+          }
+          newItem.妻.push(wife);
+        }
+      });
       return {
         ...state,
-        ...action.payload.people
+        searchedPeople: array
       };
     }
   },
