34e52f0c7a7d3c88f50a3dfcfe538744431b378f
zuopingli <studytime@163.com>
2018-08-28 23:03:24 +0800

    fixed searching bugs

--- src/services/people.js
+++ src/services/people.js
@@ -8,38 +8,38 @@ export function getSamples() {
 }
 
 export function searchByNames(originNames) {
-  if (originNames.length > 2) {
-    originNames = originNames.slice(0, 2);
-  }
+  // if (originNames.length > 2) {
+  //   originNames = originNames.slice(0, 2);
+  // }
 
   let queries = [];
   originNames.forEach(name => {
     let newName = name.replace(' ', '');
+    let queryString = '', parentName = '';
     if (newName.indexOf('>') > -1) {
       let newNames = newName.split('>');
       if (newNames.length >= 2) {
-        let parentName = newNames[newNames.length - 2];
+        parentName = newNames[newNames.length - 2];
         newName = newNames[newNames.length - 1];
-        queries.push(
-          `((son.名='${newName}' OR son.字='${newName}' OR son.号='${newName}' OR son.又='${newName}' )
-          AND
-          (parent.名='${parentName}' OR parent.字='${parentName}' OR parent.号='${parentName}' OR parent.又='${parentName}'))
-          `
-        );
       }
-    } else {
-      queries.push(
-        `(son.名='${newName}' OR son.字='${newName}' OR son.号='${newName}' OR son.又='${newName}' )`
-      );
     }
+
+    if (newName[0] === '李') {
+      newName = newName.substr(1);
+    }
+
+    queryString = `(son.名='${newName}' OR son.字='${newName}' OR son.号='${newName}' OR son.又='${newName}' )`;
+    if (parentName) {
+      queryString = `(${queryString} AND (parent.名='${parentName}' OR parent.字='${parentName}' OR parent.号='${parentName}' OR parent.又='${parentName}'))`;
+    }
+
+    queries.push(queryString);
     return newName;
   });
   const opts = {
-    query: `MATCH (son:Person)-[:RELATION*0..{role:"son"}]->(parent:Person)<-[:RELATION {role: "wife"}]-(wife:Person) WHERE ${queries.join(
-      ' OR '
-    )} RETURN distinct son, parent, wife ORDER BY id(parent) DESC`
+    query: `MATCH (son:Person)-[:RELATION*0..{role:"son"}]->(parent:Person)<-[:RELATION {role: "wife"}]-(wife:Person) WHERE ${queries.join(' OR ')} RETURN distinct son, parent, wife `
   };
-  // console.log(opts);
+  console.log(opts);
 
   return request.post(opts);
 }
--- src/store/reducers/people.js
+++ src/store/reducers/people.js
@@ -25,7 +25,7 @@ export default handleActions(
       // construct data like
       /*
       {
-        '作平': [
+        '良係>作平': [
           {名：'作平'},
           {名：'良系', 妻: [
             {名: '郭氏'}
@@ -34,36 +34,55 @@ export default handleActions(
             {名: '吴氏'}
           ]}
         ],
-        '作苏': [
+        '良俊>作苏': [
          ...
         ]
       }
       */
-      let array = {};
-      action.payload.data.data.forEach((v, i) => {
-        const son = v[0].data,
-          parent = v[1].data,
-          wife = v[2].data;
-        let name = son.名;
-        if (!array[name]) {
-          array[name] = [son];
-        }
-        const itemInArray = array[name].find(s => {
-          return s.名 === parent.名;
-        });
-        let newItem = {};
-        if (!itemInArray) {
-          newItem = parent;
-          newItem.妻 = [wife];
-          array[name].push(newItem);
-        } else {
-          newItem = itemInArray;
-          if (!newItem.妻) {
-            newItem['妻'] = [];
+      let array = {}, sons = {};
+      if (action.payload.data.data) {
+        // let parentName = '', sonName = ''; //, grandName = '';
+        action.payload.data.data.forEach((v, i) => {
+          const son = v[0].data, parent = v[1].data, wife = v[2].data;
+          let name = son.名;
+
+          // if (parent && !parentName && sonName != name && parentName != name) {
+          //   parentName = parent.名;
+          //   sonName = name;
+          // }
+          // else if (parent && parentName && !grandName) {
+          //   grandName = parent.名;
+          // }
+          // if (parentName) name = parentName + ' > ' + name;
+          // if (grandName) name = grandName + ' > ' + name;
+          if (!array[name]) {
+            array[name] = [];
+            sons[name] = son;
           }
-          newItem.妻.push(wife);
-        }
-      });
+          const itemInArray = array[name].find(s => {
+            return s.名 === parent.名;
+          });
+          let newItem = {};
+          if (!itemInArray) {
+            newItem = parent;
+            newItem.妻 = [wife];
+            array[name].push(newItem);
+            newItem.id = array[name].length;
+          } else {
+            newItem = itemInArray;
+            if (!newItem.妻) {
+              newItem['妻'] = [];
+            }
+            newItem.妻.push(wife);
+          }
+        });
+      }
+
+      for (let key in array) {
+        sons[key].id = array[key].length + 1;
+        array[key].push(sons[key]);
+      }
+
       return {
         ...state,
         searchedPeople: array
